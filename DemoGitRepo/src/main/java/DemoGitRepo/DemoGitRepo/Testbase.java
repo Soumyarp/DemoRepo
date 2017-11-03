8d921679bd847a734c3a7454372e96ebc202eeba
Soumya Ranjan <ranjan@securax.in>
2017-11-03 13:48:03 +0530

    forgot to push testbase

---
+++ DemoGitRepo/src/main/java/DemoGitRepo/DemoGitRepo/Testbase.java
@@ -0,0 +1,6 @@
+package DemoGitRepo.DemoGitRepo;
+
+public class Testbase {
+	
+
+}
---
+++ DemoGitRepo/src/test/java/DemoGitRepo/DemoGitRepo/TestDemo.java
@@ -0,0 +1,14 @@
+package DemoGitRepo.DemoGitRepo;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void testGit(){
+		
+		System.out.println("Hii");
+		
+	}
+
+}
